acc62e3d9bb6e496156d48a4ac4a280df7e200eb
Abrar Khan <abrar.khan@hee.nhs.uk>
2019-11-18 16:08:37 +0000

    Add field to claculate part of a years entitlement
    In index.html add field to enter number of clendar days remaining
    In main.js update calculateLeave function to be able to claulate leave
    for part of a years entitlement, and clean up the code to put in order
    and logical.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@ function calculateLeave() {
 
   const baseEntitlement = document.querySelector('input[name="service-length"]:checked').value;
   const weeklyHours = document.getElementById('contracted-hours').value;
+  let daysRemaining = document.getElementById('days-remaining').value;
+
+  if (daysRemaining.length == 0) {
+    daysRemaining = 365;
+  }
 
   const fte = weeklyHours / 37.5;
+  const entitlementDays = ((daysRemaining / 365) * fte * baseEntitlement);
+  const entitlementHours = entitlementDays * 7.5;
 
+  document.getElementById('result-days').innerHTML = entitlementDays.toFixed(2);
+  document.getElementById('result-hours').innerHTML = entitlementHours.toFixed(2);
   document.getElementById('result-fte').innerHTML = fte.toFixed(2);
-  document.getElementById('result-days').innerHTML = (fte * baseEntitlement).toFixed(2);
-  document.getElementById('result-hours').innerHTML = (fte * baseEntitlement * 7.5).toFixed(2);
-
-  console.log(baseEntitlement * weeklyHours);
 }
 
 
