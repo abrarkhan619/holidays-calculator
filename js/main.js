bd85b7300a1a4129e175e15cdeea40057cc1d946
Abrar Khan <abrar.khan@hee.nhs.uk>
2019-11-19 11:34:56 +0000

    Calculate pro-rata bank holiday entitlement
    Add fields in index.html to input number of bank holidays reamaining and
    the resulting bank holiday entitlements.
    Update calculateLeave funtion in main.js to include calculations for
    pro-rata days and hours bank holiday entitlement.

--- js/main.js
+++ js/main.js
@@ -3,18 +3,27 @@ function calculateLeave() {
   const baseEntitlement = document.querySelector('input[name="service-length"]:checked').value;
   const weeklyHours = document.getElementById('contracted-hours').value;
   let daysRemaining = document.getElementById('days-remaining').value;
+  let bankHolidays = document.getElementById('bank-holidays').value;
 
   if (daysRemaining.length == 0) {
     daysRemaining = 365;
   }
 
+  if (bankHolidays.length == 0) {
+    bankHolidays = 8;
+  }
+
   const fte = weeklyHours / 37.5;
   const entitlementDays = ((daysRemaining / 365) * fte * baseEntitlement);
   const entitlementHours = entitlementDays * 7.5;
+  const entitlementDaysProRata = fte * bankHolidays;
+  const entitlementHoursProRata = entitlementDaysProRata * 7.5;
 
   document.getElementById('result-days').innerHTML = entitlementDays.toFixed(2);
   document.getElementById('result-hours').innerHTML = entitlementHours.toFixed(2);
   document.getElementById('result-fte').innerHTML = fte.toFixed(2);
+  document.getElementById('pro-rata-days').innerHTML = entitlementDaysProRata.toFixed(2);
+  document.getElementById('pro-rata-hours').innerHTML = entitlementHoursProRata.toFixed(2);
 }
 
 
